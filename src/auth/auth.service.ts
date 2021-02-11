c3b7724c221703946d15d8b6403cc194e6b6ecfb
MoneyIgos <makowskiigor@gmail.com>
2021-02-11 15:14:29 +0100

    chore(auth.service): add constructor

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
-
+import { from } from 'rxjs';
+import { UsersService } from '../users/users.service';
+import * as bcrypt from 'bcrypt';
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+}
