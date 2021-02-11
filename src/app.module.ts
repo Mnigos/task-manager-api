25faf7966a7c678e8198c4bead32502c39bf8c00
MoneyIgos <makowskiigor@gmail.com>
2021-02-11 14:51:44 +0100

    chore: delete graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { GraphQLModule } from '@nestjs/graphql';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -10,9 +9,6 @@ import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot({
-      autoSchemaFile: true,
-    }),
     ConfigModule.forRoot({
       envFilePath: '.env',
       isGlobal: true,
