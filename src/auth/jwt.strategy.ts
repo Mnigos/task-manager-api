566980080da07b470930160041e8c366cc0c9551
MoneyIgos <makowskiigor@gmail.com>
2021-02-11 17:01:12 +0100

    feat(jwt.strategy): create validate method

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport';
+import { ExtractJwt } from 'passport-jwt';
+import { UsersService } from 'src/users/users.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly usersService: UsersService) {
+    super({
+      JwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
+
+  async validate(payload: any) {
+    return this.usersService.getOneByName(payload.name);
+  }
+}
