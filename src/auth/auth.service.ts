6d6b41d2e7aa88ca0e817140ee6ff0f16dae029c
MoneyIgos <makowskiigor@gmail.com>
2021-02-11 15:36:34 +0100

    feat(auth.service): add validateUser method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,27 @@
-import { Injectable } from '@nestjs/common';
-import { from } from 'rxjs';
+import { HttpStatus, Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import * as bcrypt from 'bcrypt';
+import { IUser } from 'src/users/dto/user.interface';
 @Injectable()
 export class AuthService {
   constructor(private usersService: UsersService) {}
+
+  async validateUser(user: IUser): Promise<any> {
+    const { name, pass } = user;
+    const foundedUser = await this.usersService.getOneByName(name);
+    if (
+      foundedUser !== HttpStatus.BAD_REQUEST &&
+      bcrypt.compare(pass, foundedUser.pass)
+    ) {
+      const { _id, name, createdAt } = foundedUser;
+
+      return {
+        _id,
+        name,
+        createdAt,
+      };
+    }
+
+    return null;
+  }
 }
