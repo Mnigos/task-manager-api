1cb0949ad97f34c2de9036fcc335225cc003bb05
MoneyIgos <makowskiigor@gmail.com>
2021-02-11 15:04:13 +0100

    chore(users.service): create users constructor

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { UserDoc } from './dto/user-doc.interface';
+import { IUser } from './dto/user.interface';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectModel('Users') private readonly UserModel: Model<UserDoc>
+  ) {}
+}
