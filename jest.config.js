cc887ae70ac003f05f5d0991ac2399af519a322b
MoneyIgos <makowskiigor@gmail.com>
2021-08-14 00:58:43 +0200

    chore(config): add jest config

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coveragePathIgnorePatterns: ['.*\\.(interface|module|schema).ts', 'main.ts'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+}
