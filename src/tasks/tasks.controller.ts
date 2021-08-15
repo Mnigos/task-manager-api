a3f02c195701b02c9fc777da4767a1c7c8646ee9
MoneyIgos <makowskiigor@gmail.com>
2021-08-15 13:39:45 +0200

    refactor: delete JwtAuthGuard

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -8,9 +8,7 @@ import {
   Patch,
   Post,
   Query,
-  UseGuards,
 } from '@nestjs/common'
-import { JwtAuthGuard } from '../auth/jwtAuth.guard'
 import { CreateTaskDto } from './interfaces/createTask.interface'
 import { Task } from './interfaces/task.interface'
 import { TasksService } from './tasks.service'
@@ -19,37 +17,31 @@ import { TasksService } from './tasks.service'
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
-  @UseGuards(JwtAuthGuard)
   @Get()
   async getAll(): Promise<Task[]> {
     return this.tasksService.getAll()
   }
 
-  @UseGuards(JwtAuthGuard)
   @Get('/:id')
   async getById(@Param('id') id: string): Promise<Task | HttpStatus> {
     return this.tasksService.getOneById(id)
   }
 
-  @UseGuards(JwtAuthGuard)
   @Get('/name')
   async getByName(@Query('name') name: string): Promise<Task | HttpStatus> {
     return this.tasksService.getOneByName(name)
   }
 
-  @UseGuards(JwtAuthGuard)
   @Post('/new')
   async create(@Body() task: CreateTaskDto): Promise<Task> {
     return this.tasksService.create(task)
   }
 
-  @UseGuards(JwtAuthGuard)
   @Patch('/update')
   async update(@Body() task: CreateTaskDto): Promise<Task | HttpStatus> {
     return this.tasksService.update(task)
   }
 
-  @UseGuards(JwtAuthGuard)
   @Delete('/delete/:id')
   async delete(@Body('id') id: string): Promise<{ deleted: boolean }> {
     return this.tasksService.delete(id)
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,11 @@
-import { Controller, Param, Post, UseGuards } from '@nestjs/common'
+import { Controller, Param, Post } from '@nestjs/common'
 import { UserToReturn } from '../auth/interfaces/userToReturn.interface'
-import { JwtAuthGuard } from '../auth/jwtAuth.guard'
 import { UsersService } from './users.service'
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @UseGuards(JwtAuthGuard)
   @Post('/:name')
   async getProfile(@Param('name') name: string): Promise<UserToReturn> {
     return await this.usersService.getProfile(name)
